75aea434f75c91a7fd485c82db60df9147bf5355
qa101mis <qa101mis@gmail.com>
2024-10-29 18:23:25 +0300

    commit

--- test_button.py
+++ test_button.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 def test_button_exist(driver):
     assert driver.find_element(By.ID, 'submit-id-submit').is_displayed()
 
-'''def test_button_clicked(driver):
+def test_button_clicked(driver):
     driver.find_element(By.ID, 'submit-id-submit').click()
-    assert 'Submitted' == driver.find_element(By.ID, 'result-text').text'''
+    assert 'Submitted' == driver.find_element(By.ID, 'result-text').text
     
